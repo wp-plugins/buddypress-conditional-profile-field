7da620c42bdb30bc32f7f8a3d315a79a3d2ef512
pankajanupam <pankajanupam@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-25 06:30:19 +0000

    beta release
    git-svn-id: https://plugins.svn.wordpress.org/buddypress-conditional-profile-field/trunk@523311 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ buddypress-conditional-profile-field.php
@@ -0,0 +1,30 @@
+<?php
+/*
+Plugin Name: Buddypress conditional profile field
+Plugin URI: http://pankajanupam.in/wordpress-plugins/subdomains/
+Description: Buddypress conditional profile field
+Version: 0.5
+Author: PANKAJ ANUPAM
+Author URI: http://pankajanupam.in
+
+* LICENSE
+    Copyright 2011 PANKAJ ANUPAM  (email : mymail.anupam@gmail.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+?>
+<?php
+
+?>
